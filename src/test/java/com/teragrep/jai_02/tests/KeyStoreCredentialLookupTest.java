1e8a492290045611a09e2c2454757633e4853f73
Motoko Kusanagi <kordex@gmail.com>
2024-05-07 15:50:46 +0300

    initial work on #7

---
+++ src/test/java/com/teragrep/jai_02/tests/KeyStoreCredentialLookupTest.java
@@ -0,0 +1,98 @@
+/*
+ * Java Authentication Info jai_02
+ * Copyright (C) 2021  Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://github.com/teragrep/teragrep/blob/main/LICENSE>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+
+package com.teragrep.jai_02.tests;
+
+import com.teragrep.jai_02.KeyStoreCredentialLookup;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+
+public class KeyStoreCredentialLookupTest {
+
+    private static String keyStorePath = "target/keystore.p12";
+    private static String keyStorePassword = "changeit";
+    private static String userName = "trusted-12";
+    private static String userPassWord = "XOsAqIhmKUTwWMjWwDaYmVgR8sl_l70H1oDPBw9z2yY";
+
+    @Test
+    public void saveTest() throws CertificateException, KeyStoreException, IOException, NoSuchAlgorithmException,
+            InvalidKeySpecException {
+        KeyStoreCredentialLookup.save(keyStorePath, keyStorePassword, userName, userPassWord);
+    }
+
+    @Test
+    public void readTest() throws IOException, UnrecoverableEntryException, CertificateException, KeyStoreException,
+            NoSuchAlgorithmException, InvalidKeySpecException {
+
+        InputStream inputStream = Files.newInputStream(Paths.get(keyStorePath));
+
+        boolean authOk = KeyStoreCredentialLookup.verifyKey(
+                inputStream,
+                keyStorePassword,
+                userName,
+                userPassWord
+        );
+
+        Assertions.assertTrue(authOk);
+    }
+
+    @Test
+    public void bothTest() throws CertificateException, KeyStoreException, IOException, NoSuchAlgorithmException,
+            InvalidKeySpecException, UnrecoverableEntryException {
+        saveTest();
+        readTest();
+    }
+}
+
